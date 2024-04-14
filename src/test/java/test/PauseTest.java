c1f8f6757b7909f5e07ff89d5c3a556e163bd6e8
AnimeruFR <erwan.corbin@etu.univ-lyon1.fr>
2024-04-14 10:32:28 +0200

    Test sur les modèles finis #12

---
+++ src/test/java/test/PauseTest.java
@@ -0,0 +1,16 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PauseTest {
+
+    private Pause pause = new Pause();
+    @Test
+    void setEtat() {
+        pause.setEtat(true);
+        assertEquals(pause.getEtat(), true);
+        System.out.println("SetEtat Fonctionnel");
+    }
+}
